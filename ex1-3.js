b93640c6f5f5aac96b91437624d92d1dbf63212a
hondayama <167833090+hondayama@users.noreply.github.com>
2025-01-26 15:18:58 +0900

    Create ex1-3.js

---
+++ ex1-3.js
@@ -0,0 +1,8 @@
+function hyoji31(){document.getElementById('logo3').src ='./image/43.png'; setTimeout("hyoji32()",300);}
+function hyoji32(){document.getElementById('logo3').src ='./image/7.png'; setTimeout("hyoji33()",300);}
+function hyoji33(){document.getElementById('logo3').src ='./image/26.png'; setTimeout("hyoji34()",300);}
+function hyoji34(){document.getElementById('logo3').src ='./image/82.png'; setTimeout("hyoji35()",300);}
+function hyoji35(){document.getElementById('logo3').src ='./image/43.png'; setTimeout("hyoji36()",300);}
+function hyoji36(){document.getElementById('logo3').src ='./image/o.png';}
+
+function nazo3(){ if (document.getElementById('answer3').value == '201') {alert('あたり！');} else if(document.getElementById('answer2').value == '２０１'){alert('あたり!');}　else{alert('はずれ！')}}
