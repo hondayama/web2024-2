39dad4b720220f281081d23ccb1256b8e192615d
hondayama <167833090+hondayama@users.noreply.github.com>
2025-01-26 14:33:19 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -1,6 +1,6 @@
-function hyoji11(){document.getElementById('logo').src ='./image/1.png'; setTimeout("hyoji12()",500);}
-function hyoji12(){document.getElementById('logo').src ='./image/5.png';setTimeout("hyoji13()",500);}
-function hyoji13(){document.getElementById('logo').src ='./image/7.png';setTimeout("hyoji14()",500);}
-function hyoji14(){document.getElementById('logo').src ='./image/2.png';setTimeout("hyoji15()",500);}
-function hyoji15(){document.getElementById('logo').src ='./image/8.png';setTimeout("hyoji16()",500);}
+function hyoji11(){document.getElementById('logo').src ='./image/1.png'; setTimeout("hyoji12()",300);}
+function hyoji12(){document.getElementById('logo').src ='./image/5.png'; setTimeout("hyoji13()",300);}
+function hyoji13(){document.getElementById('logo').src ='./image/7.png'; setTimeout("hyoji14()",300);}
+function hyoji14(){document.getElementById('logo').src ='./image/2.png'; setTimeout("hyoji15()",300);}
+function hyoji15(){document.getElementById('logo').src ='./image/8.png'; setTimeout("hyoji16()",300);}
 function hyoji16(){document.getElementById('logo').src ='./image/o.png';}
