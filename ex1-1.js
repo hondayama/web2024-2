80c1f045bfaf6868efbdaf6e1efa32482b863393
hondayama <167833090+hondayama@users.noreply.github.com>
2025-01-26 14:29:48 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -1,2 +1,6 @@
 function hyoji11(){document.getElementById('logo').src ='./image/1.png'; setTimeout("hyoji12()",500);}
-function hyoji12(){document.getElementById('logo').src ='./image/5.png';}
+function hyoji12(){document.getElementById('logo').src ='./image/5.png';setTimeout("hyoji13()",500);}
+function hyoji13(){document.getElementById('logo').src ='./image/7.png';setTimeout("hyoji14()",500);}
+function hyoji14(){document.getElementById('logo').src ='./image/2.png';setTimeout("hyoji15()",500);}
+function hyoji15(){document.getElementById('logo').src ='./image/8.png';setTimeout("hyoji16()",500);}
+function hyoji16(){document.getElementById('logo').src ='./image/o.png';}
