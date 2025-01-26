b7bc17e59f265af24965bd3a342797669f8d0470
hondayama <167833090+hondayama@users.noreply.github.com>
2025-01-26 14:53:17 +0900

    Update and rename ex1-2 to ex1-2.js

---
+++ ex1-2.js
@@ -0,0 +1,8 @@
+function hyoji21(){document.getElementById('logo2').src ='./image/1.png'; setTimeout("hyoji22()",300);}
+function hyoji22(){document.getElementById('logo2').src ='./image/5.png'; setTimeout("hyoji23()",300);}
+function hyoji23(){document.getElementById('logo2').src ='./image/7.png'; setTimeout("hyoji24()",300);}
+function hyoji24(){document.getElementById('logo2').src ='./image/2.png'; setTimeout("hyoji25()",300);}
+function hyoji25(){document.getElementById('logo2').src ='./image/8.png'; setTimeout("hyoji26()",300);}
+function hyoji26(){document.getElementById('logo2').src ='./image/o.png';}
+
+function nazo2(){ if (document.getElementById('answer2').value == '23') {alert('あたり！');} else{alert('はずれ!')}}
